e17b85b007ac999e01d3d7f71fe088fd0163f79a
saurabhshma <saurabhsharma0508@gmail.com>
2013-09-01 10:10:45 +0530

    linked list iterative changed

--- ll_iterative.c
+++ ll_iterative.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void create();
+void print();
+
 struct list
 {
 	int number;
@@ -14,48 +17,53 @@ node *tail;
 
 int main()
 {
-	void create(node *head);
-	void print(node *head);
 	head = (node*)malloc(sizeof(node));
-	create(head);
-	print(head);
+	create();
+	print();
 	return 0;
 }
 
-void create(node *head)
+void create()
 {
-	char choice;
+	node *temp;
+	temp = head;
 	int i = 1;
-	tail = head;
 	while(i)
 	{	
 		printf("Enter the value of node number '%d'. Press 999 at the end\n", i);
-		scanf("%d", &tail->number);
-		if(tail->number == 999) 
+		scanf("%d", &temp->number);
+		if(temp->number == 999) 
 		{
-			tail->next = NULL;
+			temp->next = NULL;
+			tail = temp;
 			printf("Linked list created successfully\n");
 			break;
 		} 
 		
 		else
 		{
-			tail->next = (node*)malloc(sizeof(node));
-			tail = tail->next;
+			temp->next = (node*)malloc(sizeof(node));
+			temp = temp->next;
 		}
 		i++;
 	}		
 }
 
-void print(node *head)
+void print()
 {
-	node *tail;
-	tail = head;
-	while(tail->next != NULL)
-	{
-		printf("%d-->", tail->number);
-		tail = tail->next;
-		if(tail->next == NULL)
-			printf("%d\n", tail->number); 
+	node *temp;
+	temp = head;
+	if(temp->next == NULL)
+	{	
+		printf("%d\nLinked list is empty\n", temp->number);
 	}
+	
+	while(temp != tail)
+	{
+		printf("%d-->", temp->number);
+		temp = temp->next;
+	}
+	printf("%d", temp->number);
+	printf("\n");
 }
+
