ea3c560ee5542b636c4f0a60442b862646866e20
Saurabh Sharma <saurabhsharma0508@gmail.com>
2014-03-26 02:09:03 +0530

    create_am corrected

--- GRAPHS/create_am.c
+++ GRAPHS/create_am.c
@@ -1,26 +1,50 @@
+#include <stdlib.h>
 #include <stdio.h>
 
+int v;
+int **A;
 void create_am();
 void display_am();
-int v;
-int A[][v];
 
 int main()
 {
+	int i, j;
+	printf("Enter number of vertices\n");
+	scanf("%d", &v);
 	create_am();
+	free(A);
 	return 0;
 }
 
 void create_am()
 {
-	int x, y, i;
-	printf("Enter Number of vertices\n");
-	scanf("%d", &v);
-	printf("Enter edges one by one\n");
+	int x, y, i, j;
+	char choice;
+	int *temp;
+	A = (int**)malloc(v * sizeof(int*));
+	temp = (int*)malloc(v * sizeof(int));
+	for (i = 0; i < v; i++) 
+	{
+		A[i] = temp + (i * v);
+	}
 	for(i = 0; i < v; i++)
 	{
+		for(j = 0; j < v; j++)
+		{
+			A[i][j] = 0;
+		}
+	}
+	while(1)
+	{
+		printf("Enter the edge\n");
 		scanf("%d %d", &x, &y);
-		A[x - 1][y - 1] = 1;	
+		A[x - 1][y - 1] = 1;
+		printf("Enter next edge? (y/n)\n");
+		scanf("\n%c", &choice);
+		if(choice == 'n')
+		{
+			break;
+		}
 	}
 	display_am();
 }
@@ -28,13 +52,13 @@ void create_am()
 void display_am()
 {
 	int i, j;
+	printf("Adjacency Matrix is:\n");
 	for(i = 0; i < v; i++)
 	{
 		for(j = 0; j < v; j++)
 		{
-			printf("%d ", A[i][j]); 
+			printf("%d ", A[i][j]);
 		}
 		printf("\n");
 	}
 }
-
