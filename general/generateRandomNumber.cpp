f2ef4fb44fd5b36aa773ae3bf5cf26742e574fad
saurabhshma <saurabhsharma0508@gmail.com>
2016-03-10 18:47:06 +0530

    Update generateRandomNumber.cpp

--- general/generateRandomNumber.cpp
+++ general/generateRandomNumber.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int getRandomNumber(int min, int max)
 {
-    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX));
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1);
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
