bae827ae8e63f66c5babfc221fd11deb6077f77c
saurabhshma <saurabhsharma0508@gmail.com>
2016-03-10 18:30:55 +0530

    Add random number generator

---
+++ CPP/test/generateRandomNumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int getRandomNumber(int min, int max)
+{
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX));
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+int main()
+{
+	srand(static_cast<int>(time(0)));
+	for(int i = 1; i <= 20; i++)
+	{
+		cout << getRandomNumber(1, 6) << " ";
+	}
+	return 0;
+}
